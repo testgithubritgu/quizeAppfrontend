76881fb0ce743013289b61475a7742b464916194
testgithubritgu <swapnilraut1698@gmail.com>
2025-07-14 08:27:31 +0530

    added apis

--- quize/src/pages/Quiz.jsx
+++ quize/src/pages/Quiz.jsx
@@ -3,6 +3,7 @@ import { useAsyncError, useNavigate } from 'react-router-dom'
 import que from "./Question.json"
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from "react-toastify"
+import axios from "axios"
 export default function Quiz() {
   const navigate = useNavigate()
   const [options, setoptions] = useState(false)
@@ -11,13 +12,37 @@ export default function Quiz() {
   const [checked, setchecked] = useState(false)
   const [afterwin, setafterwin] = useState(false)
   const [score,setscore] = useState(0)
+  const [quize,setquizedata] = useState([])
+  const [check,setcheck] = useState(null)
+  const [showletsstart,setshowletsstart] = useState(false)
+
+  const names = [
+    {name:"math"},
+    {name:"genral"},
+    {name:"coding"}
+  ]
   const logout = () => {
     localStorage.removeItem("token")
     localStorage.removeItem("user")
     navigate("/")
   }
 
-  const setgameplay = () => {
+  const fetchingQuize = async (e)=>{
+      try {
+        const res  = await axios.get(`http://localhost:5000/api/quize/${e}`)
+     console.log(res.data.e);
+     
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+
+
+  const setgameplay = (e) => {
+    
+   
+    
     setplaygame(true)
   }
 
@@ -44,9 +69,15 @@ export default function Quiz() {
     setidx(idx + 1)
   }
 
+  const getquizedata = (e)=>{
+    console.log(e)
+    
+    
+  }
+
   return (
     <>
-      <div className='fixed h-screen w-screen flex justify-center items-center bg-gradient-to-br from-slate-400 to-blue-500'>
+      <div className='fixed h-screen w-screen flex justify-center  items-center bg-gradient-to-br from-slate-400 to-blue-500'>
         <ToastContainer
           position="top-center"
           autoClose={2000}         // 2 seconds
@@ -59,6 +90,21 @@ export default function Quiz() {
           pauseOnHover={false}
           theme="dark" />
         <button onClick={() => logout()} className='p-2 border rounded-lg px-3 text-[20px] cursor-pointer absolute top-4 active:scale-95 '>Logout</button>
+        <div className='absolute  top-[70%] space-x-3 gap-2 '>
+        {!showletsstart?(<>
+          {names.map((e,i)=>{
+            return(
+              <>
+          <button onClick={()=>fetchingQuize(e.name)} className='py-2 px-4 cursor-pointer rounded-lg bg-blue-600 text-[30px] text-white '>{e.name}</button>
+              </>
+            )
+          })}
+        </>):""}
+          {/* <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button>
+          <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button>
+          <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button> */}
+        
+        </div>
         {afterwin ? (<>
 
           <div>
@@ -73,7 +119,7 @@ export default function Quiz() {
             {!playgame ? (<><div className='flex justify-center items-center flex-col '>
               <span className='text-[70px]'>Hey! Lets play the game!</span>
               <br />
-              <button onClick={() => setgameplay()} className='text-[30px]  bg-blue-600 text-white py-4 px-[10px] rounded-xl cursor-pointer active:scale-95'>Lets start😃</button>
+              <button onClick={() => setgameplay()} className={`text-[30px] ${showletsstart?"visible":"hidden"} bg-blue-600 text-white py-4 px-[10px] rounded-xl cursor-pointer active:scale-95`}>Lets start😃</button>
             </div></>) : (<>
               <h1 className='absolute top-[150px] text-[20px] text-center'>Live score <br /><span className='text-[40px]'>{score}</span></h1>
               <div className='text-center' >
@@ -86,7 +132,7 @@ export default function Quiz() {
                       <>
                         <button onClick={() => optionButtonOnclick(i)} disabled={checked} className={`py-4 px-6 m-5 text-[15px] cursor-pointer font-serif rounded-2xl ${checked ? "cursor-not-allowed" : ""} ${(checked && que[idx].answers[i].correct) ? "bg-green-500" : "bg-white"} `}>{e.option1}</button>
                       </>
-                    )
+                    ) 
                   }) : ""}
                   {/* <button className='py-4 px-6 m-5 text-[30px] cursor-pointer bg-white rounded-2xl '>2) secomd</button>
 
