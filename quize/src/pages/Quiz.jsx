68b713f08e8d633d646d4b978818cc06548468b8
testgithubritgu <swapnilraut1698@gmail.com>
2025-07-14 11:56:44 +0530

    added quize

--- quize/src/pages/Quiz.jsx
+++ quize/src/pages/Quiz.jsx
@@ -4,6 +4,10 @@ import que from "./Question.json"
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from "react-toastify"
 import axios from "axios"
+import coding from "../quizejson/codeing.json"
+import math from "../quizejson/math.json"
+import genral from "../quizejson/genral.json"
+
 export default function Quiz() {
   const navigate = useNavigate()
   const [options, setoptions] = useState(false)
@@ -11,15 +15,15 @@ export default function Quiz() {
   const [playgame, setplaygame] = useState(false)
   const [checked, setchecked] = useState(false)
   const [afterwin, setafterwin] = useState(false)
-  const [score,setscore] = useState(0)
-  const [quize,setquizedata] = useState([])
-  const [check,setcheck] = useState(null)
-  const [showletsstart,setshowletsstart] = useState(false)
-
+  const [score, setscore] = useState(0)
+  const [quize, setquizedata] = useState([])
+  const [quizeName, setquizeName] = useState(math)
+  const [showletsstart, setshowletsstart] = useState('')
+  const [checkbox, setcheckbox] = useState(0)
   const names = [
-    {name:"math"},
-    {name:"genral"},
-    {name:"coding"}
+    { name: "math" },
+    { name: "genral" },
+    { name: "coding" }
   ]
   const logout = () => {
     localStorage.removeItem("token")
@@ -27,38 +31,30 @@ export default function Quiz() {
     navigate("/")
   }
 
-  const fetchingQuize = async (e)=>{
-      try {
-        const res  = await axios.get(`http://localhost:5000/api/quize/${e}`)
-     console.log(res.data.e);
-     
-      } catch (error) {
-        console.log(error)
-      }
-  }
+
 
 
 
   const setgameplay = (e) => {
-    
-   
-    
+
+    console.log((Array.from(quizeName)))
+
     setplaygame(true)
   }
 
   const optionButtonOnclick = (i) => {
     // e.target.classList.add("cursor-not-allowed")
-    if(que[idx].answers[i].correct){
+    if (quizeName[idx].answer[i].correct) {
       toast.success("correct answer")
-      setscore(score+1)
-    }else{
+      setscore(score + 1)
+    } else {
       toast.warn("wrong answer")
     }
     setchecked(true)
   }
 
   const next = () => {
-    if (idx + 1 == que.length) {
+    if (idx + 1 == quizeName.length) {
       // setidx(0)
       setafterwin(true)
       toast.success("Thanku for completing this text!")
@@ -69,10 +65,21 @@ export default function Quiz() {
     setidx(idx + 1)
   }
 
-  const getquizedata = (e)=>{
-    console.log(e)
-    
-    
+  const checkboxinput = (i, name) => {
+    setcheckbox(i)
+
+    if (name == "math") {
+      setquizeName(math)
+    }
+    if (name == "genral") {
+
+      setquizeName(genral)
+    }
+    if (name == "coding") {
+      setshowletsstart()
+      setquizeName(coding)
+    }
+
   }
 
   return (
@@ -90,25 +97,13 @@ export default function Quiz() {
           pauseOnHover={false}
           theme="dark" />
         <button onClick={() => logout()} className='p-2 border rounded-lg px-3 text-[20px] cursor-pointer absolute top-4 active:scale-95 '>Logout</button>
-        <div className='absolute  top-[70%] space-x-3 gap-2 '>
-        {!showletsstart?(<>
-          {names.map((e,i)=>{
-            return(
-              <>
-          <button onClick={()=>fetchingQuize(e.name)} className='py-2 px-4 cursor-pointer rounded-lg bg-blue-600 text-[30px] text-white '>{e.name}</button>
-              </>
-            )
-          })}
-        </>):""}
-          {/* <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button>
-          <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button>
-          <button className='py-2 px-4  rounded-lg bg-blue-600 text-[30px] text-white '>a</button> */}
-        
-        </div>
+
         {afterwin ? (<>
 
-          <div>
-            <h1 className=' text-[60px] text-white font-mono '>{`Congratulation  your score  ${score}`}</h1>
+          <div className='text-center space-y-6'>
+            <h1 className=' text-[60px] text-white font-mono '>{`your score  ${score}/10 `}</h1>
+            <span className='text-orange-400 text-4xl'>{score<5?(<><span className='animate-pulse text-[60px]'>😕</span> its very less</>):(<><span className='animate-pulse text-[60px]'>👏</span> Pass</>)}</span>
+            <h1  className='text-[30px]'>{score<5?"I hope you do better next time — at least try to cross 5.":"Yes, you did it — you have passed!"}</h1>
 
             <button onClick={() => navigate("/")} className='block mx-auto bg-blue-400 py-3 px-3 rounded-lg cursor-pointer'>Go to home</button>
           </div>
@@ -119,21 +114,33 @@ export default function Quiz() {
             {!playgame ? (<><div className='flex justify-center items-center flex-col '>
               <span className='text-[70px]'>Hey! Lets play the game!</span>
               <br />
-              <button onClick={() => setgameplay()} className={`text-[30px] ${showletsstart?"visible":"hidden"} bg-blue-600 text-white py-4 px-[10px] rounded-xl cursor-pointer active:scale-95`}>Lets start😃</button>
+              <button onClick={() => setgameplay()} className={`text-[30px] visible bg-blue-600 text-white py-4 px-[10px] rounded-xl cursor-pointer active:scale-95`}>Lets start😃</button>
+              {names.map((e, i) => {
+                return (
+                  <div className='my-3'>
+                    <input onChange={() => checkboxinput(i, e.name)} checked={checkbox === i ? true : false} className='cursor-pointer' type="checkbox" placeholder='' /><span className='ml-2 text-white'>{e.name}</span>
+                  </div>
+                )
+              })}
             </div></>) : (<>
-              <h1 className='absolute top-[150px] text-[20px] text-center'>Live score <br /><span className='text-[40px]'>{score}</span></h1>
+              {/* <h1 className='absolute top-[150px] text-[20px] text-center'>Live score <br /><span className='text-[40px]'>{score}</span></h1> */}
               <div className='text-center' >
-                <button onClick={()=>navigate("/")}  className='text-[20px] text-white bg-gray-400 p-4 rounded-xl cursor-pointer '>Go To Home</button>
-                <h1 className='absolute top-[10%] text-red-600 text-[20px]  w-full text-left'> Don't refresh the page, otherwise you will be thrown back and start from the beginning.</h1>
-                <h1 className='text-[30px] font-semibold font-serif text-blue-200'>{que[idx].question}</h1>
-                <div className="choices mt-7  justify-center items-center  grid grid-cols-2 ">
-                  {que ? que[idx].answers.map((e, i) => {
-                    return (
-                      <>
-                        <button onClick={() => optionButtonOnclick(i)} disabled={checked} className={`py-4 px-6 m-5 text-[15px] cursor-pointer font-serif rounded-2xl ${checked ? "cursor-not-allowed" : ""} ${(checked && que[idx].answers[i].correct) ? "bg-green-500" : "bg-white"} `}>{e.option1}</button>
-                      </>
-                    ) 
-                  }) : ""}
+                <button onClick={() => navigate("/")} className='text-[20px] text-white bg-gray-400 p-4 rounded-xl cursor-pointer '>Go To Home</button>
+                <h1 className='block top-[10%] text-red-600 text-[20px]  w-full '> Don't refresh the page, otherwise you will be thrown back and start from the beginning.</h1>
+                <div className="choices mt-7  max-w-[900px]  ">
+
+                  <h1 className='text-[30px] font-semibold font-serif text-blue-200'>{`${idx + 1}) `}{quizeName[idx].question}</h1>
+                  <div className='grid grid-cols-2'>
+                    {quizeName[idx].answer.map((e, i) => {
+                      return (
+                        <>
+                          <button onClick={() => optionButtonOnclick(i)} disabled={checked} className={`py-4 px-6 m-5 text-[15px] cursor-pointer font-serif rounded-2xl ${checked ? "cursor-not-allowed" : ""} ${(checked && quizeName[idx].answer[i].correct) ? "bg-green-500" : "bg-white"} `}>{e.opt}</button>
+                        </>
+                      )
+                    })}
+
+                  </div>
+
                   {/* <button className='py-4 px-6 m-5 text-[30px] cursor-pointer bg-white rounded-2xl '>2) secomd</button>
 
               <button className='py-4 px-6 m-5 text-[30px] cursor-pointer bg-white rounded-2xl '>3) third</button>
